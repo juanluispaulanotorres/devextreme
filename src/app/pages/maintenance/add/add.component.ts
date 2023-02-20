c277dd03510b3f9121d2bb10b2be7271235d2704
juanluispaulanotorres <juanluis.paulano@bosonit.com>
2023-02-20 16:12:50 +0100

    added validation to AddComponent

--- src/app/pages/maintenance/add/add.component.ts
+++ src/app/pages/maintenance/add/add.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
@@ -12,34 +9,51 @@ import {
 export class AddComponent implements OnInit {
   addForm!: FormGroup;
 
-  countryName!: string;
-  region!: string;
-  officialLanguage!: string;
-  population!: number;
-  urlFlag!: string;
-  notes!: string;
+  // countryName!: string;
+  // region!: string;
+  // officialLanguage!: string;
+  // population!: number;
+  // urlFlag!: string;
+  // notes!: string;
 
   constructor(private builder: FormBuilder) {}
 
   ngOnInit() {
     this.addForm = this.builder.group({
-      countryName: ['', []],
-      region: ['', []],
-      officialLanguage: ['', []],
+      countryName: ['', [Validators.required]],
+      region: ['', [Validators.required]],
+      officialLanguage: ['', [Validators.required]],
       population: [
-        0,
-        [],
+        '',
+        [
+          Validators.required,
+          Validators.min(0),
+          Validators.pattern(/^-?(0|[1-9]\d*)?$/),
+        ],
       ],
-      urlFlag: ['', []],
+      urlFlag: [
+        '',
+        [
+          Validators.pattern(
+            /(^[a-zñA-ZÑ]*:\/\/[a-zñA-ZÑ]*\.[a-zA-Z]*\/?[a-z0-9]*\/[a-z]*\.[a-z]{3})/
+          ),
+        ],
+      ],
       notes: ['', []],
     });
   }
 
   fieldIsValid(campo: string) {
-    return this.addForm.controls[campo].errors && this.addForm.controls[campo].touched;
+    return (
+      this.addForm.controls[campo].errors &&
+      this.addForm.controls[campo].touched
+    );
   }
 
-  submit(formGroup: FormGroup) {
+  submit() {
+    if (this.addForm.invalid) {
+      return;
+    }
   }
 
   clear() {
