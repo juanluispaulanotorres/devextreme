1bda18fc35536c6e777b72fbc71d835b5ca544cd
juanluispaulanotorres <juanluis.paulano@bosonit.com>
2023-02-27 12:43:42 +0100

    added updating form to ModifyComponent

--- src/app/pages/country/modify/modify.component.ts
+++ src/app/pages/country/modify/modify.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import notify from 'devextreme/ui/notify';
 import { ICountry } from '../config';
 import { CountryService } from '../country.service';
 
@@ -8,6 +10,8 @@ import { CountryService } from '../country.service';
   styleUrls: ['./modify.component.scss']
 })
 export class ModifyComponent implements OnInit {
+
+  // Propiedades para la consulta
 
   data!: boolean;
   arrayCountries: ICountry[] = [];
@@ -20,14 +24,32 @@ export class ModifyComponent implements OnInit {
     urlFlag: '',
   };
 
-  constructor(private countryService: CountryService) { }
+  // Propiedades para el formulario
+  updatingFormCountry!: FormGroup;
+  numberPattern = /^\d+$/;
+  urlPattern =
+    /(^[a-zñA-ZÑ]*:\/\/[a-zñA-ZÑ]*\.[a-zA-Z]*\/?[a-z0-9]*\/[a-z]*\.[a-z]{3})/;
+
+  constructor(private _countryService: CountryService, private _builder: FormBuilder) { }
 
   ngOnInit(): void {
     this.getAllCountries();
+
+    this.updatingFormCountry = this._builder.group({
+      name: ['', [Validators.required]],
+      region: ['', [Validators.required]],
+      language: ['', [Validators.required]],
+      population: [
+        '',
+        [Validators.required, Validators.pattern(this.numberPattern)],
+      ],
+      urlFlag: ['', [Validators.pattern(this.urlPattern)]],
+      description: ['', []],
+    });
   }
 
   getAllCountries() {
-    this.countryService
+    this._countryService
       .getListCountries()
       .subscribe((countries: ICountry[]) => {
         countries.forEach((country: ICountry) => {
@@ -43,15 +65,59 @@ export class ModifyComponent implements OnInit {
 
     this.arrayCountries.forEach((country) => {
       if (countryName === country.name) {
-        this.country.name = country.name;
-        this.country.region = country.region;
-        this.country.language = country.language;
-        this.country.population = country.population;
-        this.country.urlFlag = country.urlFlag;
-        this.country.description = country.description;
+        setTimeout(() => {
+          this.country.name = country.name;
+          this.country.region = country.region;
+          this.country.language = country.language;
+          this.country.population = country.population;
+          this.country.urlFlag = country.urlFlag;
+          this.country.description = country.description;
+        }, 1);
 
         this.data = true;
       }
     });
+  }
+
+  submit() {
+    if (this.updatingFormCountry.invalid) {
+      return;
+    } else {
+      // ENVIAR DATOS A BACK
+      const dataForm: ICountry = this.updatingFormCountry.value;
+
+      this._countryService.addCountry(dataForm).subscribe(
+        () => {
+          notify(
+            {
+              message: 'You have submitted the form',
+              position: {
+                my: 'center top',
+                at: 'center top',
+              },
+            },
+            'success',
+            3000
+          );
+        },
+        (error) => {
+          notify(
+            {
+              message: 'It has been a problem with the form',
+              position: {
+                my: 'center top',
+                at: 'center top',
+              },
+            },
+            'error',
+            3000
+          );
+        }
+      );
+    }
+  }
+
+  clear() {
+    this.updatingFormCountry.reset();
   }
 }
