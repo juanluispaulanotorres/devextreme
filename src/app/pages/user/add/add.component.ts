0cb5144edef6b69797b028a6282f75ff63e119cb
juanluispaulanotorres <juanluis.paulano@bosonit.com>
2023-03-07 13:56:12 +0100

    implemented user add form and added inputs

--- src/app/pages/user/add/add.component.ts
+++ src/app/pages/user/add/add.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  addFormUser!: FormGroup;
 
-  constructor() { }
+  urlPattern =
+    /(^[a-zñA-ZÑ]*:\/\/[a-zñA-ZÑ]*\.[a-zA-Z]*\/?[a-z0-9]*\/[a-z]*\.[a-z]{3})/;
 
-  ngOnInit(): void {
+  // Buttons
+  // countryComponent = new CountryComponentsConfig();
+  // arrayButtons = this.countryComponent.buttons;
+
+  constructor(
+    private _builder: FormBuilder
+  ) // private _userService: UserService
+  {}
+
+  ngOnInit() {
+    this.addFormUser = this._builder.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      prefix: ['', [Validators.required]],
+      position: ['', [Validators.required]],
+      picture: ['', [Validators.pattern(this.urlPattern)]],
+      birthDate: [''],
+      hireDate: [''],
+      address: [''],
+      notes: ['', []],
+    });
   }
-
 }
