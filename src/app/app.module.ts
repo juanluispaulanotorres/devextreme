df6bd0c170fb933a5ddc5de8007e59e3266a5d94
juanluispaulanotorres <juanluis.paulano@bosonit.com>
2023-03-07 08:26:47 +0100

    created module 'user'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,12 +19,14 @@ import { UnauthenticatedContentModule } from './unauthenticated-content';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CountryModule } from './pages/country/country.module';
+import { UserModule } from './pages/user/user.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     CountryModule,
+    UserModule,
     HttpClientModule,
     SideNavOuterToolbarModule,
     SideNavInnerToolbarModule,
